f722ae9ca3a631847204afd71cd235b7bd92b793
Charlie Paterson <Charlieed.paterson@googlemail.com>
2019-11-15 10:57:32 +0000

    Built and tested storage function for List class

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,7 +1,16 @@
 class Birthday
+  def initialize
 
+  end
 end
 
 class List
+attr_reader :dates
+  def initialize
+    @dates = []
+  end
 
+  def store(bday)
+    @dates << bday
+  end
 end
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -2,7 +2,12 @@ require "birthday_list"
 
 describe List do
 
- end
+#Can our list store birthdays?
+list = List.new
+bday = Birthday.new
+list.store(bday)
+it{ expect(list.dates).to include(bday)}
+end
 
 describe Birthday do
 
